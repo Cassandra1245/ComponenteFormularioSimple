88be6ebaecd1ac5ebd5cadb5893358b583562f0a
Cassandra1245 <guillermogarciasuarez@gmail.com>
2024-11-24 14:30:57 +0100

    ProyectoCompletado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [nombre, setNombre] = useState("");
+
+  const handleChange = (event) => {
+    setNombre(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" value={nombre} onChange={handleChange} placeholder="Introduzca su nombre:" />
+        {nombre && <p>Hola, {nombre}</p>}
       </header>
     </div>
   );
